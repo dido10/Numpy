675cdbb48ab6b212986224860c750cd26bc15e44
dido10 <didodurmaz@hotmail.com>
2019-05-02 14:54:57 +0300

    Add files via upload

---
+++ vektorandmatrices.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May  2 14:25:35 2019
+
+@author: didem.durmaz
+"""
+import numpy as np
+
+L=[[1,2],[2,3]]
+M=np.array([[1,2],[3,4]])
+
+
+M[0]        #array([1, 2])
+M[0][0]        #1
+
+L[1]        #array([2, 3])
+M[1][1]     #4
+
+
+M2=np.matrix([[1,2],[3,4]])
+
+A=np.array(M2)
+A.T   #array([[1, 3],
+     #        [2, 4]])
