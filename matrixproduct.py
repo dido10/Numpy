d57536e7deba113fc5939a92da63bea252f5efd7
dido10 <didodurmaz@hotmail.com>
2019-05-06 16:32:42 +0300

    Add files via upload

---
+++ matrixproduct.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+a=np.zeros(10)
+
+b=np.zeros((10,10))
+
+c=np.ones((10,10))
+
+d=np.random.random((10,10))
+
+e=np.random.randn(10,10)    #gaussian distributed numbers mean=0 variance=1
+e.mean()
+e.var()
